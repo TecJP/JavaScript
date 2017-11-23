339176430a3d19dfb417f0cf97e6fcfdc9b0da39
TecJP <tec.joao.freitas@gmail.com>
2017-11-22 22:26:50 -0200

    Adicionado arquivo separado do javascript

---
+++ _Aula_1/js/script.js
@@ -0,0 +1,5 @@
+console.log(
+    /*Aqui no javascript o código vai chamar o elemento div que estiver com o ID "time-grande" através do getElementById*/
+    document.getElementById("time-grande").innerHTML
+    /*No console do inspecionar navegador, será mostrado o conteúdo da div com id "time-grande" que é Corinthians*/
+)
